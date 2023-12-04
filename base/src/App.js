07982730631c47c5343dd7bf58a16f85595f7ba9
Tphilus <tkdadzie@st.ug.edu.gh>
2023-12-04 23:15:34 +0000

    feat: react fundametals (map, destructuring object)

--- base/src/App.js
+++ base/src/App.js
@@ -5,19 +5,25 @@ const books = [
     author: "Jordan Moore",
     title: "Interesting Facts For Curious Minds",
     img: "./images/book-1.jpg",
+    id: 1,
   },
   {
     author: "James Clear",
     title: "Atomic Habits",
     img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+    id: 2,
   },
 ];
 
 const BookList = () => {
-  return <section className="booklist"> </section>;
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
 };
-
-cont names = []
 
 const Book = ({ img, title, author }) => {
   return (
