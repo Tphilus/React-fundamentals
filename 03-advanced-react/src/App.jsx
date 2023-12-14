b9694d130f15435fa5617c8a3e296eed144bbbc5
Tphilus <tkdadzie@st.ug.edu.gh>
2023-12-14 21:36:41 +0000

    feat: update leverage and forms

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-// import Starter from "./tutorial/01-useState/starter/05-useState-gotcha";
+import Starter from "./tutorial/06-forms/starter/01-controlled-inputs";
 
 // USEFFECT
 // import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
@@ -7,13 +7,11 @@
 // import { Home, About } from "./tutorial/04-project-structure/starter/Pages";
 
 import "./App.css";
-import List from "./tutorial/05-leverage-javascript/starter/List";
 
 function App() {
   return (
     <div className="container">
-      <h1> Leverage Javascript </h1>
-      <List />
+      <Starter />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ 03-advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/prop-types */
 // import React from "react";
-export function Person({ name, nickName, images }) {
-  const img = images[0].small.url;
+import avatar from "../../../assets/default-avatar.svg";
+export function Person({ name, nickName = "shakeAndBake", images }) {
+  // const img = images?.[0]?.small?.url || avatar;
+  const img = images?.[0]?.small?.url ?? avatar;
   return (
     <div>
-      <img src="" alt={name} style={{ width: "50px" }} />
+      <img src={img} alt={name} style={{ width: "50px" }} />
       <h4> {name} </h4>
       <p> Nickname : {nickName} </p>
     </div>
