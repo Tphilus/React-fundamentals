dfd7abbb7621e8fb95fff86190a7caf65ac846c9
Tphilus <tkdadzie@st.ug.edu.gh>
2023-12-17 22:15:47 +0000

    feat: forms update

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/06-forms/starter/01-controlled-inputs";
+import Starter from "./tutorial/06-forms/starter/05-form-data";
 
 // USEFFECT
 // import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
