a06489bae15114dee38efc1ba7d6cdbff1878193
Tphilus <tkdadzie@st.ug.edu.gh>
2023-12-28 20:50:58 +0000

    update: 07-slider

--- 04-fundamental-projects/07-slider/starter/src/App.jsx
+++ 04-fundamental-projects/07-slider/starter/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
@@ -6,20 +6,45 @@ import longList from "./data";
 const App = () => {
   const [people, setPeople] = useState(longList);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
-    <section className=" slick-container">
+    <section className="slider-container">
       <div className="title">
         <h2>
           <span>/</span> reviews
         </h2>
       </div>
-      <div className="slider-container">
+      <div className=" section-center">
         {longList.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length)
+          ) {
+            position = "lastSlide";
+          }
           console.log(person);
           // more stuff coming up
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={title} className="person-img" />
               <h4>{name}</h4>
               <p className="title"> {title} </p>
@@ -28,11 +53,11 @@ const App = () => {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           {" "}
           <FiChevronLeft />{" "}
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           {" "}
           <FiChevronRight />{" "}
         </button>
