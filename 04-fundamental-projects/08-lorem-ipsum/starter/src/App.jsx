6c5bba7a71edb250203883b7cbc717fcabca8edb
Tphilus <tkdadzie@st.ug.edu.gh>
2023-12-29 22:03:15 +0000

    update

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,44 @@
+import { useState } from "react";
+import data from "./data";
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(typeof count);
+    let amount = perseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h4> tired of boring Lorem Ipsum? </h4>
+      <form action="" className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraph:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={() => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text?.map((item, index) => {
+          return <p key={index}> {item} </p>;
+        })}
+      </article>
+    </section>
+  );
 };
 export default App;
