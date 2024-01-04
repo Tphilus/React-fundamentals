9af5e79deb3d9f9053156cbbec6ec1d875607942
Tphilus <tkdadzie@st.ug.edu.gh>
2024-01-03 17:07:44 -0800

    feat: update

--- 04-fundamental-projects/11-navbar/starter/src/Navbar.jsx
+++ 04-fundamental-projects/11-navbar/starter/src/Navbar.jsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTwitter } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 
 export default function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
   return (
     <nav>
       <div className="nav-header">
         <img src={logo} alt="logo" />
-        <button className="nav-toggle">
+        <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
           <FaBars />
         </button>
       </div>
-      <div className="links-container show-container">
+      { showLinks && <div className="links-container show-container">
         <ul className="links">
           {links?.map((link) => {
             const { id, url, text } = link;
@@ -23,7 +24,20 @@ export default function Navbar() {
             );
           })}
         </ul>
-      </div>
+      </div>}
+
+      {/* <div className={`${ showLinks ? "links-container show-container" : "links-container"}`} >
+        <ul className="links">
+          {links?.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div> */}
       <ul className="social-icons">
         {social?.map((socialIcon) => {
           const { id, url, icon } = socialIcon;
