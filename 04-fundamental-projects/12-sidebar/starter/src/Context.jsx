fed1044a3f73eb8485072acd5be2df3ad4e89228
Tphilus <tkdadzie@st.ug.edu.gh>
2024-01-05 00:35:53 -0800

    update: done with the sidebar and modal

---
+++ 04-fundamental-projects/12-sidebar/starter/src/Context.jsx
@@ -0,0 +1,48 @@
+import { createContext, useContext, useState } from "react";
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Sidebar func
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  /// Modal func
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        closeModal,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// Custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
---
+++ 04-fundamental-projects/12-sidebar/starter/src/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "./Context";
+
+export default function Home() {
+  // const data = useContext(AppContext);
+  const {openSidebar, openModal} = useGlobalContext();
+  console.log(openSidebar, openModal);
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal} >show modal</button>
+    </main>
+  );
+}
