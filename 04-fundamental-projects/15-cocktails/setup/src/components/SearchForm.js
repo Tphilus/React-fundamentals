03b7f09e91f89dc0cd7db1d0865768bdb8324d03
Tphilus <tkdadzie@st.ug.edu.gh>
2024-01-11 17:09:05 +0000

    feat: create navbar,About page, Error page, home page

--- 04-fundamental-projects/15-cocktails/setup/src/components/SearchForm.js
+++ 04-fundamental-projects/15-cocktails/setup/src/components/SearchForm.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React, { useRef } from "react";
+import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
-}
+  const { setSearchTerm } = useGlobalContext();
 
-export default SearchForm
+  const searchValue = useRef("");
+
+  return (
+    <section className="section search">
+      <form action="" className="search-form">
+        {" "}
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
--- 04-fundamental-projects/15-cocktails/setup/src/pages/About.js
+++ 04-fundamental-projects/15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Aliquip ut eiusmod pariatur officia do eu.Velit sit et nulla ex non
+        labore qui occaecat. Adipisicing commodo aliquip aliqua non sit cillum.
+        Aliqua nisi do in sint excepteur nulla enim. Pariatur excepteur Lorem et
+        amet mollit non est sunt velit mollit. Sit est commodo ut sint consequat
+        aliquip. Esse fugiat esse reprehenderit ullamco cillum. Lorem sunt minim
+        fugiat dolore consectetur commodo aliquip non aute minim dolor officia.
+        Deserunt tempor sint nostrud laboris reprehenderit consequat.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
