d91af4ea7c9ca70638b5db7c4daa9cfbfc104c1e
Tphilus <tkdadzie@st.ug.edu.gh>
2024-01-12 18:28:11 +0000

    feat: query param

--- 04-fundamental-projects/15-cocktails/setup/src/components/SearchForm.js
+++ 04-fundamental-projects/15-cocktails/setup/src/components/SearchForm.js
@@ -1,15 +1,34 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
-
   const searchValue = useRef("");
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <section className="section search">
-      <form action="" className="search-form">
-        {" "}
+      <form action="" className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name"> search your favirate cocktail </label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
       </form>
     </section>
   );
--- 04-fundamental-projects/15-cocktails/setup/src/pages/SingleCocktail.js
+++ 04-fundamental-projects/15-cocktails/setup/src/pages/SingleCocktail.js
@@ -1,14 +1,86 @@
-import React from 'react'
-import Loading from '../components/Loading'
-import { useParams, Link } from 'react-router-dom'
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+import React, { useEffect, useState } from "react";
+import Loading from "../components/Loading";
+import { useParams, Link } from "react-router-dom";
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SingleCocktail = () => {
-  return (
-    <div>
-      <h2>single cocktail page </h2>
-    </div>
-  )
-}
+  const { id } = useParams();
+  const [loading, setLoading] = useState();
+  const [cocktail, setCocktail] = useState(null);
 
-export default SingleCocktail
+  useEffect(() => {
+    setLoading(true);
+
+    async function getCocktail() {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+
+        if (data.drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0];
+
+          const ingredient = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredient,
+          };
+
+          setCocktail(newCocktail);
+        } else {
+          setCocktail(null);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
+    getCocktail();
+  }, [id]);
+
+  if (loading) return <Loading />;
+
+  if (!cocktail)
+    return <h2 className="section-title"> no cocktail to display </h2>;
+
+  const { name, image, category, info, glass, instructions, ingredient } =
+    cocktail;
+  return (
+    <section className="section cocktail-section">
+      <Link to="/" className="btn btn-primary">
+        back home
+      </Link>
+      <h2 className="section-title"> {name} </h2>
+      <div className="drink">
+        <img src={image} alt={name} />
+        <div className="drink-info"></div>
+      </div>
+    </section>
+  );
+};
+
+export default SingleCocktail;
