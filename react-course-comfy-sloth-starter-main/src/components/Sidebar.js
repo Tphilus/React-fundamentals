7f5bc4469b0ede0d361e0a1a24e9c916db8dcaae
Tphilus <tkdadzie@st.ug.edu.gh>
2024-01-18 06:51:48 +0000

    update: context and the about

--- react-course-comfy-sloth-starter-main/src/components/Sidebar.js
+++ react-course-comfy-sloth-starter-main/src/components/Sidebar.js
@@ -9,15 +9,16 @@ import CartButtons from "./CartButtons";
 import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
-  const isOpen = false;
-  const data = useProductsContext();
-  console.log(data);
+  const { isSidebarOpen, closeSidebar } = useProductsContext();
+
   return (
     <SidebarContainer>
-      <aside className={`${isOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <aside
+        className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}
+      >
         <div className="sidebar-header">
           <img src={logo} alt="comfy sloth" className="logo" />
-          <button className="close-btn">
+          <button className="close-btn" onClick={closeSidebar}>
             <FaTimes />
           </button>
         </div>
@@ -26,12 +27,16 @@ const Sidebar = () => {
             const { id, text, url } = link;
             return (
               <li key={id}>
-                <Link to={url}> {text} </Link>
+                <Link to={url} onClick={closeSidebar}>
+                  {text}
+                </Link>
               </li>
             );
           })}
           <li>
-            <Link to="/checkout"> checkout </Link>
+            <Link to="/checkout" onClick={closeSidebar}>
+              checkout
+            </Link>
           </li>
         </ul>
         <CartButtons />
--- react-course-comfy-sloth-starter-main/src/pages/AboutPage.js
+++ react-course-comfy-sloth-starter-main/src/pages/AboutPage.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero />
+      <Wrapper className="page section section-center ">
+        <img src={aboutImg} alt="nice desk " />
+        <article>
+          <div className="title">
+            <h2> our story </h2>
+            <div className="underline"></div>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +46,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
