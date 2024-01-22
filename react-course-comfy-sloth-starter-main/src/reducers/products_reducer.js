0bc04a89bb897affa9b5d0559529f0b49cc2dbea
Tphilus <tkdadzie@st.ug.edu.gh>
2024-01-22 19:17:01 +0000

    update: context:products_context and reducers:products_reducer

--- react-course-comfy-sloth-starter-main/src/reducers/products_reducer.js
+++ react-course-comfy-sloth-starter-main/src/reducers/products_reducer.js
@@ -17,6 +17,27 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false };
   }
+
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    );
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_products,
+    };
+  }
+
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
+
   return state;
   throw new Error(`No Matching "${action.type}" - action type`);
 };
