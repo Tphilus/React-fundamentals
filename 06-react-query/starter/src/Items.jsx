c1013c69a869061acea0605e13ece16167e81202
Tphilus <tkdadzie@st.ug.edu.gh>
2024-04-12 10:42:18 -0700

    feat: react-query for fetching

--- 06-react-query/starter/src/Items.jsx
+++ 06-react-query/starter/src/Items.jsx
@@ -1,8 +1,30 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import { useQuery } from "@tanstack/react-query";
+import SingleItem from "./SingleItem";
+import customFetch from "./utils";
+const Items = () => {
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const { data } = await customFetch.get("/something");
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}> Loading... </p>;
+  }
+
+  // if (isError) {
+  //   return <p style={{ marginTop: "1rem" }}>{error.message}</p>;
+  // }
+
+  if (error) {
+    return <p style={{ marginTop: "1rem" }}>{error.response.data}</p>;
+  }
+
   return (
-    <div className='items'>
-      {items.map((item) => {
+    <div className="items">
+      {data.data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
