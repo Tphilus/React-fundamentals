bf39f4e93034761dcc5db95b876304f31f6b7c95
Tphilus <tkdadzie@st.ug.edu.gh>
2024-04-13 16:45:20 -0700

    update

--- 06-react-query/server/server.js
+++ 06-react-query/server/server.js
@@ -1,35 +1,35 @@
-import express from 'express';
-import cors from 'cors';
-import { nanoid } from 'nanoid';
+import express from "express";
+import cors from "cors";
+import { nanoid } from "nanoid";
 const app = express();
-import morgan from 'morgan';
+import morgan from "morgan";
 
 let taskList = [
-  { id: nanoid(), title: 'walk the dog', isDone: false },
-  { id: nanoid(), title: 'wash dishes', isDone: false },
-  { id: nanoid(), title: 'drink coffee', isDone: true },
-  { id: nanoid(), title: 'take a nap', isDone: false },
+  { id: nanoid(), title: "walk the dog", isDone: false },
+  { id: nanoid(), title: "wash dishes", isDone: false },
+  { id: nanoid(), title: "drink coffee", isDone: true },
+  { id: nanoid(), title: "take a nap", isDone: false },
 ];
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
 }
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello From Server...</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Hello From Server...</h1>");
 });
 
-app.get('/api/tasks', (req, res) => {
+app.get("/api/tasks", (req, res) => {
   res.json({ taskList });
 });
 
-app.post('/api/tasks', (req, res) => {
+app.post("/api/tasks", (req, res) => {
   const { title } = req.body;
   if (!title) {
-    res.status(400).json({ msg: 'please provide title' });
+    res.status(400).json({ msg: "please provide title" });
     return;
   }
   const newTask = { id: nanoid(), title, isDone: false };
@@ -37,7 +37,7 @@ app.post('/api/tasks', (req, res) => {
   res.json({ task: newTask });
 });
 
-app.patch('/api/tasks/:id', (req, res) => {
+app.patch("/api/tasks/:id", (req, res) => {
   const { id } = req.params;
   const { isDone } = req.body;
 
@@ -48,17 +48,17 @@ app.patch('/api/tasks/:id', (req, res) => {
     return task;
   });
 
-  res.json({ msg: 'task updated' });
+  res.json({ msg: "task updated" });
 });
 
-app.delete('/api/tasks/:id', (req, res) => {
+app.delete("/api/tasks/:id", (req, res) => {
   const { id } = req.params;
   taskList = taskList.filter((task) => task.id !== id);
 
-  res.json({ msg: 'task removed' });
+  res.json({ msg: "task removed" });
 });
 
-app.use((req, res) => res.status(404).send('Route does not exist'));
+app.use((req, res) => res.status(404).send("Route does not exist"));
 
 const port = process.env.PORT || 5000;
 
