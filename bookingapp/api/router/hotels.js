76c57bd298c24196ce23c511c4b6dc44169d0aa1
Tphilus <tkdadzie@st.ug.edu.gh>
2024-06-07 14:55:46 -0700

    update

--- bookingapp/api/router/hotels.js
+++ bookingapp/api/router/hotels.js
@@ -1,24 +1,33 @@
 import express from "express";
 import {
+    countByCity,
+    countByType,
     createHotel,
     deleteHotel,
     getHotel,
+    getHotelRooms,
     getHotels,
     updateHotel,
 } from "../controllers/hotel.js";
-import { verifyAdmin } from "../utils/verifyToken.js";
-
-
+import Hotel from "../models/Hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js"
 const router = express.Router();
-// CREATE
+
+//CREATE
 router.post("/", verifyAdmin, createHotel);
-// UPDATE
+
+//UPDATE
 router.put("/:id", verifyAdmin, updateHotel);
-// DELETE
+//DELETE
 router.delete("/:id", verifyAdmin, deleteHotel);
-// GET
-router.get("/:id", getHotel);
-// GET ALL
+//GET
+
+router.get("/find/:id", getHotel);
+//GET ALL
+
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 
 export default router;
